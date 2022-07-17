fb74dcc6aac925cf90e536a278769e1c770347b1
Andrew Pham <locpp2001@gmail.com>
2022-07-17 10:23:21 +0700

    set up project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React, {
+  useEffect, useState, useMemo, useRef,
+} from "react";
+
+function App() {
+  return (
+    <div className="App" />
+  );
+}
+
+export default App;
